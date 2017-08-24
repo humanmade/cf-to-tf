a5b34057973091484d20130cfc1fdaee619cfee3
Nathaniel Schweinberg <natanielks@gmail.com>
2017-08-24 14:44:03 -0700

    update Stack keymapping to allow for non-required properties as well as defaults

--- tests/configuration.js
+++ tests/configuration.js
@@ -12,4 +12,32 @@ describe('', () => {
 			expect(result).toEqual(expected);
 		});
 	});
+
+	test('does not break when paremeters field is not present', () => {
+		const cf = require('./fixtures/describe-stacks.json');
+		const expected = require('./fixtures/example.tf.json');
+		delete cf.Stacks[0].Parameters;
+		delete expected.resource.aws_cloudformation_stack.network.parameters;
+		const opts = {
+			stack: 'lambda-monitoring',
+			resourceName: 'network',
+		};
+		return configuration.generate(opts, cf.Stacks[0]).then(result => {
+			expect(result).toEqual(expected);
+		});
+	});
+
+	test('sets different default value when it needs a different one', () => {
+		let cf = require('./fixtures/describe-stacks.json');
+		let expected = require('./fixtures/example.tf.json');
+		delete cf.Stacks[0].Capabilities;
+		delete expected.resource.aws_cloudformation_stack.network.capabilities;
+		const opts = {
+			stack: 'lambda-monitoring',
+			resourceName: 'network',
+		};
+		return configuration.generate(opts, cf.Stacks[0]).then(result => {
+			expect(result).toEqual(expected);
+		});
+	});
 });
